a6bf32876c37a14c9f952b9152cbe8701c981a88
Ryuki Tsushida <r.tsushida.sys21@morijyobi.ac.jp>
2022-03-22 10:48:06 +0900

    ファイル編集

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ def top_page();
 
     if __name__ =="__ main__";
         app.run(debug=True)
+
+from flask import Flask, render_template, request
+
+@app.route("/circle_input")
+def circle_input();
+    return render_template("circle_input.html")
+
+@app.route("/circle_result")
+def circle_result();
+    radius = int(request.args.get("radius"))
+	result = 3.14 * radius ** 2
+	return render_template("circle_result.html", result=result)
